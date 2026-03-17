c8c458730d8e6e80356c67cca5fa9468ee5d2d8d
TranMC <tranmc2005@gmail.com>
2026-03-17 16:03:07 +0700

    feat: Mini project

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,13 +2,37 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-	public GameObject enemyPrefab;
+    public enum SpawnMode { Manual, Auto }
 
-	void Update()
-	{
-    	if(Input.GetKeyDown(KeyCode.Space))
-    	{
-        	Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-    	}
-	}
+    [Header("Spawn Mode")]
+    public SpawnMode spawnMode = SpawnMode.Manual;
+
+    [Header("Manual Mode")]
+    public GameObject enemyPrefab;
+
+    [Header("Auto Mode")]
+    public GameObject[] enemyPrefabs;
+    public float spawnTime = 2f;
+
+    void Start()
+    {
+        if (spawnMode == SpawnMode.Auto)
+        {
+            InvokeRepeating("SpawnEnemy", 1f, spawnTime);
+        }
+    }
+
+    void Update()
+    {
+        if (spawnMode == SpawnMode.Manual && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int index = Random.Range(0, enemyPrefabs.Length);
+        Instantiate(enemyPrefabs[index], transform.position, Quaternion.identity);
+    }
 }
